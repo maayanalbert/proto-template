9bab786e40eb733971aa285c6d136150f2eb4caa
Maayan Albert <maayan.albert@gmail.com>
2026-07-19 16:23:20 -0400

    Create page.tsx

---
+++ src/app/component-library/page.tsx
@@ -0,0 +1,7 @@
+import { createPrototypeComponentLibraryPage } from "proto-plugin";
+
+export default createPrototypeComponentLibraryPage({
+  syncConfigPath: "prototype.sync.config.sh",
+  componentLibraryPagePath:
+    "src/app/component-library/component-library-content.tsx",
+});
